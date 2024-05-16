f81cb7a0f63644e391f9f43254ac2a01fe02e089
Juan Daza <jc.dazam1@uniandes.edu.co>
2024-05-15 19:19:59 -0500

    Agregar Models

---
+++ Models/EmployeeApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AmarisTestApp.Models
+{
+    public class EmployeeApiResponse
+    {
+        public string Status { get; set; }
+        public List<Employee> Data { get; set; } // Cambiamos el tipo de Data a List<Employee>
+        public string Message { get; set; }
+    }
+}
