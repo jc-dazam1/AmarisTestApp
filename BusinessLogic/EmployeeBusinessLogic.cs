c2a3b4ed0dd7f95e0ed0452bfa853ef01e6ae74b
Juan Daza <jc.dazam1@uniandes.edu.co>
2024-05-15 19:35:53 -0500

    Crear Logica de Negocio, pendiente por usar el controller

---
+++ BusinessLogic/EmployeeBusinessLogic.cs
@@ -0,0 +1,29 @@
+﻿using AmarisTestApp.Models;
+
+namespace AmarisTestApp.BusinessLogic
+{
+    public class EmployeeBusinessLogic
+    {
+        /// <summary>
+        /// Calculate the annual Salary
+        /// </summary>
+        /// <param name="monthlySalary"></param>
+        /// <returns></returns>
+        public decimal CalculateAnnualSalary(decimal monthlySalary)
+        {
+            // Calcula el salario anual multiplicando el salario mensual por 12
+            return monthlySalary * 12;
+        }
+
+        /// <summary>
+        /// Get the Salary of an Employee 
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public decimal CalculateAnnualSalary(Employee employee)
+        {
+            // Pendiente por modificar
+            return CalculateAnnualSalary(employee.EmployeeSalary);
+        }
+    }
+}
