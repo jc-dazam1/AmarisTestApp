0a71c16c4cd6c173d8a169b75a43bc11bcb78590
Juan Daza <jc.dazam1@uniandes.edu.co>
2024-05-15 19:47:45 -0500

    Crear El controller, y modificar el bussiness logic para usar ese controler.

--- BusinessLogic/EmployeeBusinessLogic.cs
+++ BusinessLogic/EmployeeBusinessLogic.cs
@@ -1,9 +1,39 @@
-﻿using AmarisTestApp.Models;
+﻿using AmarisTestApp.Controllers;
+using AmarisTestApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AmarisTestApp.BusinessLogic
 {
     public class EmployeeBusinessLogic
     {
+        private readonly EmployeeController _employeeController;
+
+        public EmployeeBusinessLogic(EmployeeController employeeController)
+        {
+            _employeeController = employeeController;
+        }
+
+        /// <summary>
+        /// Get the Salary of an Employee 
+        /// </summary>
+        /// <param name="idEmployee"></param>
+        /// <returns></returns>
+        public async Task<decimal> CalculateAnnualSalary(int idEmployee)
+        {
+            var employeeResponse = await _employeeController.GetEmployee(idEmployee);
+
+            if (employeeResponse.Result is OkObjectResult)
+            {
+                var employee = (employeeResponse.Result as OkObjectResult).Value as Employee;
+                return CalculateAnnualSalary(employee.EmployeeSalary);
+            }
+            else
+            {
+                // Handle error response
+                throw new Exception($"Failed to retrieve employee {idEmployee}. Status code: {employeeResponse.Result}");
+            }
+        }
+
         /// <summary>
         /// Calculate the annual Salary
         /// </summary>
@@ -13,17 +43,6 @@ namespace AmarisTestApp.BusinessLogic
         {
             // Calcula el salario anual multiplicando el salario mensual por 12
             return monthlySalary * 12;
-        }
-
-        /// <summary>
-        /// Get the Salary of an Employee 
-        /// </summary>
-        /// <param name="employee"></param>
-        /// <returns></returns>
-        public decimal CalculateAnnualSalary(Employee employee)
-        {
-            // Pendiente por modificar
-            return CalculateAnnualSalary(employee.EmployeeSalary);
         }
     }
 }
